5326b0cfc937079701a4c4d693812476a09c38f4
Taric <jmd865859@gmail.com>
2026-06-10 23:45:48 +0600

    feat(cli): adding cli entry module
    - Creating cli.py file.

---
+++ src/weight_tracker/cli.py
@@ -0,0 +1,13 @@
+"""Main module for cli tool.
+
+After it call as cli tool this module will start runing.
+
+Functions:
+    run_app(): The main function.
+"""
+
+#TODO: add imports
+
+
+def run_app() -> None:
+    pass #TODO: adding function calls
